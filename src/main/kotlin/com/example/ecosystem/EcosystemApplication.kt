1adb4fbacf546d6da653ccdf115884f13f5501af
misha <mabotov2002@gmail.com>
2024-10-18 19:47:22 +0300

    Исправление сборки gradle

--- src/main/kotlin/com/example/ecosystem/EcosystemApplication.kt
+++ src/main/kotlin/com/example/ecosystem/EcosystemApplication.kt
@@ -1,9 +1,10 @@
 package com.example.ecosystem
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  [ DataSourceAutoConfiguration::class ])
 class EcosystemApplication
 
 fun main(args: Array<String>) {
