d34f6d54fae0e4546382e3315a6d7125a5a84f6f
misha <mabotov2002@gmail.com>
2024-10-18 18:27:45 +0300

    Инициализация проекта

---
+++ src/main/kotlin/com/example/ecosystem/EcosystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ecosystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcosystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcosystemApplication>(*args)
+}
